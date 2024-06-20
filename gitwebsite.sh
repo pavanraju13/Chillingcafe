a061c0629ae182311ea9d3ee5e67d22e7342055e
root <root@ip-172-31-18-17.ec2.internal>
2024-06-20 12:26:18 +0000

    This is a new website

---
+++ gitwebsite.sh
@@ -0,0 +1,21 @@
+#/!bin/bash
+
+git clone git@github.com:pavanraju13/Chillingcafe.git
+
+rm -rf ~/pavanraju13/Chillingcafe.git/*
+
+wget https://www.tooplate.com/zip-templates/2118_chilling_cafe.zip
+
+sudo yum install unzip -y
+
+unzip 2118_chilling_cafe.zip
+
+cp -r 2118_chilling_cafe/* Chillingcafe/
+
+git status
+
+git add .
+
+git commit -m "This is a new website"
+
+git push
